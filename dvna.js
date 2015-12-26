018e52e9fc564e02e91ce3923df3d4069fac161c
Michael Pinter <mpinter09@gmail.com>
2015-12-25 20:39:48 -0800

    added ascii DVNA

--- dvna.js
+++ dvna.js
@@ -4,7 +4,13 @@ DVNA.get('/', function(req, res) {
 });
 DVNA.listen(6666);
 
-// TODO: Create ASCII art for console management
+console.log("____________   ___________      _____   ");
+console.log("\\______ \\   \\ /   /\\      \\    /  _  \\  ");
+console.log(" |    |  \\   Y   / /   |   \\  /  /_\\  \\ ");
+console.log(" |    `   \\     / /    |    \\/    |    \\");
+console.log("/_______  /\\___/  \\____|__  /\\____|__  /");
+console.log("        \\/                \\/         \\/ ");
 console.log("\r\n   Damn Vulnerable Node Application ");
+
 console.log("  https://github.com/quantumfoam/DVNA \r\n");
 console.log("DVNA listening at: https://127.0.0.1:6666/");
