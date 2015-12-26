8a86c06f8c2a7d2f9537bbd9d50b1b9958c5c303
Michael Pinter <mpinter09@gmail.com>
2015-12-26 10:36:44 -0800

    changed to epic font

--- dvna.js
+++ dvna.js
@@ -4,12 +4,16 @@ DVNA.get('/', function(req, res) {
 });
 DVNA.listen(6666);
 
-console.log("____________   ___________      _____   ");
-console.log("\\______ \\   \\ /   /\\      \\    /  _  \\  ");
-console.log(" |    |  \\   Y   / /   |   \\  /  /_\\  \\ ");
-console.log(" |    `   \\     / /    |    \\/    |    \\");
-console.log("/_______  /\\___/  \\____|__  /\\____|__  /");
-console.log("        \\/                \\/         \\/ ");
+console.log("   ______            _        _______ ");
+console.log("  (  __  \\ |\\     /|( (    /|(  ___  )");
+console.log("  | (  \\  )| )   ( ||  \\  ( || (   ) |");
+console.log("  | |   ) || |   | ||   \\ | || (___) |");
+console.log("  | |   | |( (   ) )| (\\ \\) ||  ___  |");
+console.log("  | |   ) | \\ \\_/ / | | \\   || (   ) |");
+console.log("  | (__/  )  \\   /  | )  \\  || )   ( |");
+console.log("  (______/    \\_/   |/    )_)|/     \\|");
+                                    
+
 console.log("\r\n   Damn Vulnerable Node Application ");
 
 console.log("  https://github.com/quantumfoam/DVNA \r\n");
